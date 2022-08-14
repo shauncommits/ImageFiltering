5b1c3290cc226db32ec18b1de921451a97bbfd2c
shauncommits <shaunmbolompo426@gmail.com>
2022-08-14 10:50:24 +0200

    Added comments on the Experiment.py

--- Experiment.py
+++ Experiment.py
@@ -1,15 +1,19 @@
 import os
 import subprocess
 
-imageList = ["img1.jpg","img4.jpg","img3.jpg","img5.jpg","img2.jpg"]
+# list of the images I used from smallest to biggest in size
+imageList = ["img1.jpg","img4.jpg","img3.jpg","img5.jpg","img2.jpg"] 
 
-def process(run,windowSize):
-    for img in imageList:
-        for i in range(0,3):
+# method to run the programs and take parameters of the program to be ran and the window size to be used in that program
+def process(run,windowSize): 
+    # loop through the list of images 
+    for img in imageList: 
+        # for each image I run the program and window size received as arguments 5 times to determine the optimum time the program takes to execute
+        for i in range(0,5): 
             os.system("java -cp bin "+run+ " "+img+" "+img+" "+str(windowSize))
 
 
-
+# call the the method with the following arguments for each of the 4 programs
 process("MedianFilterSerial",3)
 process("MedianFilterParallel",3)
 process("MeanFilterSerial",3)
